ca547cc2e7b966571dfaef9ce08305181c9500c9
oliver <oliver@kyrintech.com>
2018-03-14 18:37:10 +0800

    薄荷

---
+++ src/command/boohee/api.js
@@ -0,0 +1,49 @@
+// http://food.boohee.com/fb/v1/foods/fast_search?q=咖啡
+// http://ifood.boohee.com/v2/ifoods/keywords?token=ZC9PNaY6US4AdnvefCrM
+// 
+// http://record.boohee.com/api/v2/weights/latest?token=ZC9PNaY6US4AdnvefCrM 体重
+// http://messenger.boohee.com/api/v2/feedbacks/check_unread 回馈
+// http://one.boohee.com/api/v1/notifications/unread_count?token=ZC9PNaY6US4AdnvefCrM&status_api_version=1.3 通告
+// http://bingo.boohee.com/api/v1/native_pushes?etag=d0b595377775673a39cc55ebbbc7615f&token=ZC9PNaY6US4AdnvefCrM 更新
+/**
+ * http://passport.boohee.com/api/v1/users/25524891?sign=0MSy5IRa0RpxgJMrdDXBQ33%2BTD0%3D&app_key=one&context_params=eyJ0b2tlbiI6IlpDOVBOYVk2VVM0QWRudmVmQ3JNIiwidXNlcl9rZXkiOiI4YjIzY2M5NC03NDk1LTQzNDgtOTAwNC05ZTYyNDM2NzE2MTMifQ%3D%3D 用户信息
+ * http://passport.boohee.com/api/v1/start_app_devices
+ * http://status.boohee.com/api/v1/app_square/start_up_with_ad 广告
+ * http://record.boohee.com/api/v2/weight_details/show?token=ZC9PNaY6US4AdnvefCrM 体重
+ * http://bingo.boohee.com/api/v1/home/items2?token=ZC9PNaY6US4AdnvefCrM 项目
+ * http://record.boohee.com/api/v2/weights/recent?token=ZC9PNaY6US4AdnvefCrM 最近
+ * http://status.boohee.com/api/v1/bet_nats/underway?token=ZC9PNaY6US4AdnvefCrM 导航
+ * http://bingo.boohee.com/api/v2/sports_user_records/show?token=ZC9PNaY6US4AdnvefCrM 记录
+ * http://record.boohee.com/api/v2/user_habits?token=ZC9PNaY6US4AdnvefCrM 习惯
+ * http://record.boohee.com/api/v2/user_bands/utensils?token=ZC9PNaY6US4AdnvefCrM 餐具
+ * http://status.boohee.com/api/v1/channels/categories?token=ZC9PNaY6US4AdnvefCrM 分类
+ * http://status.boohee.com/api/v1/grasses?token=ZC9PNaY6US4AdnvefCrM
+ * 热门活动
+ * 
+ * http://status.boohee.com/api/v2/posts/infinite?ad=1&token=ZC9PNaY6US4AdnvefCrM
+ * 发现信息？
+ * 
+ * http://status.boohee.com/api/v1/home_timeline?token=ZC9PNaY6US4AdnvefCrM
+ * 主页信息
+ * 
+ * http://one.boohee.com/api/v1/carts/count?token=ZC9PNaY6US4AdnvefCrM
+ * 购物车信息
+ * 
+ * http://one.boohee.com/api/v1/shop_home?version=v5&token=ZC9PNaY6US4AdnvefCrM
+ * 商店信息
+ * 
+ * http://one.boohee.com/api/v1/orders/stats?token=ZC9PNaY6US4AdnvefCrM
+ * 不知道什么信息
+ * 
+ * http://food.boohee.com/fb/v1/foods/kafeinaihu
+ * 咖啡奶糊的信息
+ * 
+ * http://ifood.boohee.com/v2/favorite_foods?token=ZC9PNaY6US4AdnvefCrM
+ * 喜欢吃的食物
+ * 
+ * http://status.boohee.com/api/v1/user_profile/detail?token=ZC9PNaY6US4AdnvefCrM
+ * 获取个人信息
+ * 
+ * http://food.boohee.com/fb/v1/foods/fast_search?q=咖啡
+ * 查询咖啡
+ */
