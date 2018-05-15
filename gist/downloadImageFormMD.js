d683207ee6ef5dcdf0bf25f08c2ea0928e2f77ac
bubao <asd565586630@gmail.com>
2018-05-15 15:21:37 +0800

    gist

--- gist/downloadImageFormMD.js
+++ gist/downloadImageFormMD.js
@@ -1,15 +1,15 @@
-let fs = require('fs');
-let request = require("request");
-let _ = require("lodash");
-let path = require('path');
+const fs = require('fs');
+const request = require("request");
+
+const path = require('path');
 /** 
  * 读取本地文件 1.md
  * 下载到imgs文件夹下
 */
-let md = fs.readFileSync("./1.md", 'utf8');
-let all = md.match(/\!\[\]\(https.*?\)/g);
+const md = fs.readFileSync("./1.md", 'utf8');
+const all = md.match(/!\[\]\(https.*?\)/g);
 
-let loop = (arr) => {
+const loop = (arr) => {
 	// if (arr.length) {
 	// 	request(arr[0]).pipe(fs.createWriteStream(`imgs/${path.basename(arr[0])}`)).on('close', () => {
 	// 		loop(_.slice(arr, 1));
@@ -17,9 +17,9 @@ let loop = (arr) => {
 	// } else {
 	// 	console.log('end');
 	// }
-	for (let index = 0; index < arr.length; index++) {
+	for (let index = 0; index < arr.length; index += 1) {
 		request(arr[index]).pipe(fs.createWriteStream(`imgs/${path.basename(arr[index])}`));
 	}
 }
-loop(JSON.parse(JSON.stringify(all).replace(/\!\[\]\(/g, "").replace(/\)/g, "")));
+loop(JSON.parse(JSON.stringify(all).replace(/!\[\]\(/g, "").replace(/\)/g, "")));
 
--- gist/imooc-crawler.js
+++ gist/imooc-crawler.js
@@ -1,16 +1,18 @@
-let url = "https://www.imooc.com/course/programdetail/pid/"
-let request = require('request');
-let fs = require('fs');
+const url = "https://www.imooc.com/course/programdetail/pid/"
+const request = require('request');
+const fs = require('fs');
 
-let cr = (i) => {
+const cr = (i) => {
 	request(url + i, (error, response, body) => {
-		if (!error && response.statusCode == 200) {
-			fs.writeFile(`./c/${i}.html`, JSON.stringify(body), (error, data) => {
-				console.log(data);
+		if (!error && response.statusCode === 200) {
+			fs.writeFile(`./c/${i}.html`, JSON.stringify(body), (err, data) => {
+				if (!err) {
+					console.log(data);
+				}
 			});
 		}
 	})
 }
-for (let i = 0; i < 100; i++) {
+for (let i = 0; i < 100; i += 1) {
 	cr(i);
 }
