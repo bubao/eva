faa86b3a531b14be3e95006ecb6501835f7066e1
bubao <asd565586630@gmail.com>
2018-11-11 01:47:40 +0800

    delete

--- src/modules/zhihu/src/api/Answer.js
+++
@@ -1,7 +0,0 @@
-/**
- * @author bubao 
- * @description 
- * @date: 2018-05-17 13:47:58 
- * @Last Modified by: bubao
- * @Last Modified time: 2018-05-17 13:59:29
- */
--- src/modules/zhihu/src/api/Question.js
+++
@@ -1,7 +0,0 @@
-/**
- * @author bubao 
- * @description 
- * @date: 2018-05-17 13:48:39 
- * @Last Modified by: bubao 
- * @Last Modified time: 2018-05-17 13:48:39 
- */
