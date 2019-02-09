32fb855d23fe03f590835e99cc0fcdeda2be6066
bubao <asd565586630@gmail.com>
2019-02-09 20:55:15 +0800

    u

--- src/command/xmly/index.js
+++ src/command/xmly/index.js
@@ -3,11 +3,11 @@
  * @description 喜马拉雅下载器
  * @date: 2018-11-11 10:54:30
  * @Last Modified by: bubao
- * @Last Modified time: 2019-02-09 20:23:47
+ * @Last Modified time: 2019-02-09 20:53:46
  */
 
 const { DownTracks, DownAlbums } = require('xmly');
-const { console, figlet, filenamify, path, fs } = require('../../tools/commonModules');
+const { console, figlet, path, fs } = require('../../tools/commonModules');
 
 async function ximalaya(type, ID, filename) {
 	console.log(figlet.textSync(`XMLY`, {
@@ -16,7 +16,7 @@ async function ximalaya(type, ID, filename) {
 		verticalLayout: 'default'
 	}));
 	console.log(`-----🐛 ${ID} start -----`);
-	filename = path.resolve(filenamify(filename, "⛰"));
+	filename = path.resolve(filename);
 	const func = type === "albums" ? DownAlbums : DownTracks;
 	console.log(`filename:${filename}\nID:${ID}\ntype:${type}`)
 	fs.writeFileSync(filename, await func(ID - 0));
