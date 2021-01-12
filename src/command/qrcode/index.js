a72f4d0524b6a530fea15dcb062f394b9e21cc7f
bubao <asd565586630@gmail.com>
2021-01-12 21:08:39 +0800

    修复🐛(command): 特性✨(command): wifi qrcode
    - fix prompt
    - use qrcode terminal

--- src/command/qrcode/index.js
+++ src/command/qrcode/index.js
@@ -2,20 +2,20 @@
  * @Description:
  * @Author: bubao
  * @Date: 2019-12-23 18:40:02
- * @LastEditors: bubao
- * @LastEditTime: 2020-06-28 15:43:00
+ * @last author: bubao
+ * @last edit time: 2021-01-12 20:47:00
  */
 // const chalk = require("chalk");
 const QRCode = require("qrcode");
 const { clicolor } = require("../../tools/commonModules");
 
 function qrcode(str) {
-	QRCode.toString(str, (err, str) => {
+	QRCode.toString(str, { type: "terminal" }, (err, str) => {
 		if (err) {
 			console.log(`Generate qrcode error: ${clicolor.red.bold(err)}.`);
 			return;
 		}
-		console.log(clicolor.blue(str));
+		console.log(str);
 	});
 }
 
